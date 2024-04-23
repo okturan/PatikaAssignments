4d8420ea744401089c40e0e636ca9e58457f7e3a
okturan <okanert@gmail.com>
2024-04-23 16:20:40 +0300

    Added operations order assignment.

---
+++ src/week1/AritmetikIslemOnceligi.java
@@ -0,0 +1,23 @@
+package week1;
+
+import java.util.Scanner;
+
+public class AritmetikIslemOnceligi {
+    public static void main ( String[] args ) {
+        int a, b, c, result;
+        Scanner input = new Scanner ( System.in );
+
+        System.out.print ( "Birinci sayıyı giriniz: " );
+        a = input.nextInt ();
+
+        System.out.print ( "İkinci sayıyı giriniz: " );
+        b = input.nextInt ();
+
+        System.out.print ( "Üçüncü sayıyı giriniz: " );
+        c = input.nextInt ();
+
+        result = a + b * c - b;
+
+        System.out.println ( "İşlem sonucu: " + result );
+    }
+}
