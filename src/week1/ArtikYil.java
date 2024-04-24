20c1357a64fc3d47816c39b0585b453cc35218a6
okturan <okanert@gmail.com>
2024-04-24 22:58:05 +0300

    Added comments.

--- src/week1/ArtikYil.java
+++ src/week1/ArtikYil.java
@@ -11,9 +11,14 @@ public class ArtikYil {
         System.out.print ( "Yıl giriniz: " );
         year = input.nextInt ();
 
-        if ( year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) {
+        /* Eğer yıl hem 4'e tam bölünüyor hem de 100'e bölümünden kalan 0 ise Artık yıldır.
+           Bir de bundan bağımsız olarak 400'e tam bölünüyorsa da artık yıldır.
+        */
+        if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ) {
             System.out.println ( year + " bir artık yıldır." );
         } else {
+
+            // Bu kriterlerin hiçbirine uymuyorsa artık yıl değildir.
             System.out.println ( year + " bir artık yıl değildir." );
         }
     }
