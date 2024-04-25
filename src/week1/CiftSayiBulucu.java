9f58f99b4fdf6e6a435bb8325a82002efb8e348e
okturan <okanert@gmail.com>
2024-04-25 11:09:24 +0300

    Updated code to match patika assignment.

--- src/week1/CiftSayiBulucu.java
+++ src/week1/CiftSayiBulucu.java
@@ -4,22 +4,33 @@ import java.util.Scanner;
 
 public class CiftSayiBulucu {
     public static void main ( String[] args ) {
-        int x;
+        int inputNumber;
         Scanner input = new Scanner ( System.in );
 
         System.out.print ( "Bir sayı giriniz: " );
-        x = input.nextInt ();
+        inputNumber = input.nextInt ();
 
         // Bundan sonra yalnızca çift sayı basılacağını bildiğimiz için ön bilgiyi burada veriyoruz.
-        System.out.println ("Girdiğiniz sayıya kadar olan tüm çift sayılar: ");
+        System.out.println ( "Girdiğiniz sayıya kadar olan tüm çift sayılar: " );
 
         // 0'dan başlayarak girdiğimiz sayıya kadar her sayıyı işliyoruz.
-        for (int i = 0; i <= x; i++) {
+        for (int i = 0; i <= inputNumber; i++) {
 
             // Bu sayılardan yalnızca 2'ye tam bölünebilenlerini print alıyoruz.
             if ( i % 2 == 0 ) {
                 System.out.println ( i );
             }
+
         }
+
+        //Bonus içerik. Patika ödevinde istenen fonksiyonu sonradan ekliyorum:
+        int total = 0, counter = 0;
+        for (int i = 0; i <= inputNumber; i++) {
+            if ( i % 3 == 0 && i % 4 == 0 ) {
+                total += i;
+                counter++;
+            }
+        }
+        System.out.println ( "Girdiğiniz sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalaması: " + total / counter );
     }
 }
