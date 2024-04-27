3a61e1601b2c3eccfb5b9e1b7ab0988c62f58850
okturan <okanert@gmail.com>
2024-04-27 10:46:14 +0300

    Refactored using Euclidean algorithm.

--- src/week1/EbokEkok.java
+++ src/week1/EbokEkok.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class EbokEkok {
 	public static void main ( String[] args ) {
-		int n, k, ebob = 0, ekok;
+		int n1, n2, ebob, ekok;
 		Scanner input = new Scanner ( System.in );
 
-		System.out.print ( "n için bir değer giriniz: " );
-		n = input.nextInt ();
+		System.out.print ( "n1 için bir değer giriniz: " );
+		n1 = input.nextInt ();
 
-		System.out.print ( "k için bir değer giriniz: " );
-		k = input.nextInt ();
+		System.out.print ( "n2 için bir değer giriniz: " );
+		n2 = input.nextInt ();
 
-		int i = 1;
-		while ( i <= n ) {
-			/*
-			n'ye kadar sırayla her sayıyı iki değerimize de bölüyoruz.
-			İkisinden de kalan 0 ise ortak bölendir. Son atanan değer ise en büyük ortak bölendir.
-			*/
-			if ( n % i == 0 && k % i == 0 ) {
-				ebob = i;
-			}
-			i++;
+		int dividend = n1;
+		int divisor = n2;
+		int remainder = 1;
+
+		// Öklid algoritmasına göre ebob bulunması:
+
+		while ( remainder != 0 ) { // Kalan 0 olana kadar bu döngü çalışacak
+			remainder = dividend % divisor; // İki sayının birbirine bölümünden kalan bulunur.
+			dividend = divisor; // Bu seferki bölen sayı sonraki döngü için bölünen olarak atanır.
+			divisor = remainder; // Kalan sayı sonraki döngü için bölen olarak atanır.
 		}
 
+		ebob = dividend;
+
 		// Formüle göre EKOK hesaplıyoruz.
-		ekok = (n * k) / ebob;
-		System.out.println ( n + " ve " + k + " sayılarının en büyük ortak böleni = " + ebob );
-		System.out.println ( n + " ve " + k + " sayılarının en küçük ortak katı = " + ekok );
+		ekok = (n1 * n2) / ebob;
+		System.out.println ( n1 + " ve " + n2 + " sayılarının en büyük ortak böleni = " + ebob );
+		System.out.println ( n1 + " ve " + n2 + " sayılarının en küçük ortak katı = " + ekok );
 	}
 }
