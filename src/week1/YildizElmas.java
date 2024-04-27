b0f4eabbbd533e8bfe186c72e28d84eb80656e9f
okturan <okanert@gmail.com>
2024-04-27 08:40:28 +0300

    Refactor

--- src/week1/YildizElmas.java
+++ src/week1/YildizElmas.java
@@ -13,18 +13,27 @@ public class YildizElmas {
 			n = input.nextInt ();
 		} while ( n % 2 == 0 || n < 2 );
 
-		// Elmasın orta noktasını hesapla.
-		middle = ((n - 1) / 2) + 1;
+		// Elmasın orta noktasını hesapla. (0 indexli kabul ediyoruz)
+		middle = n / 2;
 
 		// Dış döngü her bir satır için çalışır.
-		for (int lineNumber = 1; lineNumber <= n; lineNumber++) {
-			// İç döngü satırdaki her bir karakter için çalışır.
-			for (int charIndex = 1; charIndex <= n; charIndex++) {
-				// Eğer mevcut konum, hesaplanan elmas alanının dışındaysa "-" bas.
-				if ( middle > n - (Math.abs ( charIndex - middle ) + Math.abs ( lineNumber - middle )) ) {
-					System.out.print ( "- " );
+		for (int lineNumber = 0; lineNumber < n; lineNumber++) {
+
+			// Dikey mesafeyi hesapla
+			int verticalDistance = Math.abs ( lineNumber - middle );
+
+			// İç döngü her karakter için çalışır.
+			for (int charIndex = 0; charIndex < n; charIndex++) {
+
+				// Yatay Mesafeyi hesapla
+				int horizontalDistance = Math.abs ( charIndex - middle );
+
+				// Anlık pozisyon orta noktaya oranla kontrol edilir. Sınırlar dahilinde değilse boşluk basılır.
+				if ( horizontalDistance + verticalDistance > middle ) {
+					System.out.print ( "  " );
 				} else {
-					// Diğer durumlarda "*" bas.
+
+					// Sınırlar dahiline girdiğinde yıldız basılır.
 					System.out.print ( "* " );
 				}
 			}
