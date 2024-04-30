43bc27beb6b0e94ca9714f4eb51e75c42ac7cd2f
okturan <okanert@gmail.com>
2024-04-30 11:46:27 +0300

    added a class for recursively generating and printing a symmetric pattern of numbers.

---
+++ src/week2/RecursivePatternGenerator.java
@@ -0,0 +1,23 @@
+package week2;
+
+import java.util.Scanner;
+
+public class RecursivePatternGenerator {
+	public static void main ( String[] args ) {
+		Scanner input = new Scanner ( System.in );
+
+		System.out.print ( "Bir sayı giriniz: " );
+		int number = input.nextInt ();
+
+		printSymmetricSequence ( number );
+	}
+
+	public static void printSymmetricSequence ( int number) {
+		System.out.print(number + " "); // Mevcut sayıyı yazdırır.
+
+		if (number > 0) {
+			printSymmetricSequence ( number - 5 ); // Fonksiyonu sayıyı 5 azaltarak kendini çağırır.
+			System.out.print ( number + " " ); // Geriye sardığında o sıradaki sayı kaç ise onu yazdırır.
+		}
+	}
+}
