91b2eeb9794f144dff1e7983f44b43489a0752b0
okturan <okanert@gmail.com>
2024-05-05 21:02:22 +0300

    linting

--- src/week2/MatrixTransposer.java
+++ src/week2/MatrixTransposer.java
@@ -1,9 +1,9 @@
 package week2;
 
 public class MatrixTransposer {
-	public static void main ( String[] args ) {
+	public static void main (String[] args) {
 		int[][] matrix = {{4, 9, 20}, {10, 1, 102}, {66, 33, 11}};
-		matrixPrinter ( matrix );
+		matrixPrinter (matrix);
 		int[][] matrixTransposed = new int[matrix[0].length][matrix.length];
 
 		for (int row = 0; row < matrix.length; row++) {
@@ -11,12 +11,13 @@ public class MatrixTransposer {
 				matrixTransposed[col][row] = matrix[row][col];
 			}
 		}
-		matrixPrinter ( matrixTransposed );
+		matrixPrinter (matrixTransposed);
 	}
-	public static void matrixPrinter ( int[][] array ) {
+
+	public static void matrixPrinter (int[][] array) {
 		for (int[] row : array) {
 			for (int element : row) {
-				System.out.print ( element + "\t" );
+				System.out.print (element + "\t");
 			}
 			System.out.println ();
 		}
