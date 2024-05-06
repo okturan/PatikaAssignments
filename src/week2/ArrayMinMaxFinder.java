dc5f8418058d7964d085cda9e9bc58df1351fce7
okturan <okanert@gmail.com>
2024-05-06 18:10:26 +0300

    refactored and added comments

--- src/week2/ArrayMinMaxFinder.java
+++ src/week2/ArrayMinMaxFinder.java
@@ -4,31 +4,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayMinMaxFinder {
-	public static void main ( String[] args ) {
+	public static void main (String[] args) {
 		int[] list = {0, 56, 1, 34, 0, 2, 12, 8, -2, 101, -3, 0, 5, -1, -33};
-		Scanner input = new Scanner ( System.in );
+		Scanner input = new Scanner(System.in);
 
-		System.out.print ( "Enter a number: " );
-		int referenceNumber = input.nextInt ();
+		System.out.print("Enter a number: ");
+		int referenceNumber = input.nextInt();
 
-		int minNumber = 0, maxNumber = 0;
+		int minNumber = 0, maxNumber = 0; // minimum ve maksimum sayıları 0'dan başlatıyoruz.
 
 		for (int i : list) {
-			if ( i > referenceNumber ) {
-				if ( maxNumber == 0 || i < maxNumber ) {
-					maxNumber = i;
-				}
-			} else if ( i < referenceNumber ) {
-				if ( minNumber == 0 || i > minNumber ) {
-					minNumber = i;
-				}
+			// Sıradaki eleman girilen sayıdan büyükse ve;
+			// Maksimum sayıdan küçükse (Başlangıç değerinin 0 olduğu koşulu da dahil ediyoruz || ile)
+			if ((i > referenceNumber) && (maxNumber == 0 || i < maxNumber)) {
+				maxNumber = i; // Yeni maksimum sayımız bu olur.
+
+				// Sıradaki eleman girilen sayıdan küçükse ve;
+				// Minimum sayıdan büyükse (Başlangıç değerinin 0 olduğu koşulu da dahil ediyoruz || ile)
+			} else if ((i < referenceNumber) && (minNumber == 0 || i > minNumber)) {
+				minNumber = i; // Yeni minimum sayımız bu olur.
 			}
 		}
-
-		System.out.println ( "In the array of: " + Arrays.toString ( list ) );
-		System.out.println ( "The nearest number above " + referenceNumber + " is "
-							 + (maxNumber == 0 ? "not found" : maxNumber) + "." );
-		System.out.println ( "The nearest number below " + referenceNumber + " is "
-							 + (minNumber == 0 ? "not found" : minNumber) + "." );
+		// Arrayi konsola yazdırıyoruz:
+		System.out.println("In the array of: " + Arrays.toString(list));
+		// Sonrasında ternary operator ile max veya min hala 0 ise "not found" yorumunu yazıyoruz.
+		System.out.println("The nearest number above " + referenceNumber + " is " + (maxNumber == 0 ? "not found" : maxNumber) + ".");
+		System.out.println("The nearest number below " + referenceNumber + " is " + (minNumber == 0 ? "not found" : minNumber) + ".");
 	}
 }
