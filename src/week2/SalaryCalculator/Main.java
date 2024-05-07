05a83677180caa0d4233d2be8059869f4f74bb82
okturan <okanert@gmail.com>
2024-05-07 15:55:25 +0300

    refactor and added comments

--- src/week2/SalaryCalculator/Main.java
+++ src/week2/SalaryCalculator/Main.java
@@ -1,18 +1,22 @@
 package week2.SalaryCalculator;
 
-import static java.lang.System.*;
-
 public class Main {
-	public static void main ( String[] args ) {
-		Employee e1 = new Employee ( "Kemal", 2000, 1985, 45 );
+	public static void main (String[] args) {
+		// Çalışan nesnesi oluşturuluyor. İsim, maaş, işe alma yılı ve çalışma saatleri parametre olarak veriliyor
+		Employee e1 = new Employee("Kemal", 2000, 1985, 45);
 
-		out.println (e1.toString ());
+		// Çalışanın bilgilerini yazdır
+		System.out.println(e1.toString());
 
-		out.println ( "Tax = " + e1.tax () );
-		out.println ( "Bonus = " + e1.bonus () );
-		out.println ( "Salary raise = " + (e1.raiseSalary ()) );
-		out.println ( "Salary after tax and bonus added = " + (e1.salary + e1.bonus () - e1.tax ()) );
-		out.println ( "Salary after raise = " + (e1.salary + e1.bonus () - e1.tax () + e1.raiseSalary ()) );
-
+		// Vergiyi hesapla ve yazdır
+		System.out.println("Tax = " + e1.tax());
+		// Bonusu hesapla ve yazdır
+		System.out.println("Bonus = " + e1.bonus());
+		// Maaş zammını hesapla ve yazdır
+		System.out.println("Salary raise = " + e1.raiseSalary());
+		// Vergi ve bonus eklenmiş maaşı hesapla ve yazdır
+		System.out.println("Salary after tax and bonus added = " + (e1.salary + e1.bonus() - e1.tax()));
+		// Zam sonrası maaşı hesapla ve yazdır. Bu maaş, vergi çıkarıldıktan ve bonus eklenip zam uygulandıktan sonra elde edilen miktardır.
+		System.out.println("Salary after raise = " + (e1.salary + e1.bonus() - e1.tax() + e1.raiseSalary()));
 	}
 }
